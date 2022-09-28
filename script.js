8818a94f8899eec7dfd73b6f920255dc109bdbeb
Godwin Daniel <lemuelgodwin2@gmail.com>
2022-09-28 14:23:44 +0100

    Javascript added

---
+++ script.js
@@ -0,0 +1,19 @@
+open = ()=>{
+    const op=document.getElementsByClassName("open");
+    if (op.style.display==="block"){
+        op.style.display="none"
+        cl.style.display="block"
+    }
+    else{
+        op.style.display="block"
+        cl.style.display="none"
+    }
+}
+
+// close => () =>{
+//     const cl=document.getElementsByClassName("close");
+//     if(cl.style.display==="block"){
+//         cl.style.display==="none"
+//     }
+// }
+
