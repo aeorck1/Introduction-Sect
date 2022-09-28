cf1cddb564a0ed4071ebc4a3375f3e8911291d22
Godwin Daniel <lemuelgodwin2@gmail.com>
2022-09-28 15:17:21 +0100

    Javascript added

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-open = ()=>{
-    const op=document.getElementsByClassName("open");
-    if (op.style.display==="block"){
-        op.style.display="none"
-        cl.style.display="block"
+// oPen = ()=>{
+//     const op=document.getElementsByClassName("open");
+//     const cl=document.getElementsByClassName("close");
+//     if (op.style.display==="block"&&cl.style.display==="none"){
+//         op.style.display="none"
+//         cl.style.display="block"
+//     }
+//     else{
+//         op.style.display="block"
+//         cl.style.display="none"
+//     }
+// }
+
+function oPen() {
+    var x = document.getElementById("open");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
-    else{
-        op.style.display="block"
-        cl.style.display="none"
-    }
-}
+  }
 
 // close => () =>{
 //     const cl=document.getElementsByClassName("close");
